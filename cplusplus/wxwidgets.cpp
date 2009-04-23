0584b8b8b290953edabad734cc0631c52d0c7165
Will Roe <willroe@gmail.com>
2009-04-23 21:21:21 +0100

    stuff

---
+++ cplusplus/wxwidgets.cpp
@@ -0,0 +1,68 @@
+#include "wx/wx.h"
+
+enum
+{
+  ID_Quit = 1,
+  ID_About,
+};
+
+class MyFrame: public wxFrame
+{
+public:
+
+  MyFrame(const wxString& title, const wxPoint& pos, const wxSize& size);
+
+  void OnQuit(wxCommandEvent& event);
+  void OnAbout(wxCommandEvent& event);
+
+  DECLARE_EVENT_TABLE()
+};
+
+MyFrame::MyFrame(const wxString& title, const wxPoint& pos, const wxSize& size)
+  : wxFrame((wxFrame *)NULL, -1, title, pos, size)
+{
+  wxMenu *menuFile = new wxMenu;
+
+  menuFile->Append( ID_About, _T("&About...") );
+  menuFile->AppendSeparator();
+  menuFile->Append( ID_Quit, _T("E&xit") );
+
+  wxMenuBar *menuBar = new wxMenuBar;
+  menuBar->Append( menuFile, _T("&File") );
+
+  SetMenuBar( menuBar );
+
+  CreateStatusBar();
+  SetStatusText( _T("Welcome to wxWidgets!") );
+}
+
+void MyFrame::OnAbout(wxCommandEvent& WXUNUSED(event))
+{
+  wxMessageBox(_T("This is a wxWidgets Hello world sample"),
+	       _T("About Hello World"), wxOK | wxICON_INFORMATION, this);
+}
+
+void MyFrame::OnQuit(wxCommandEvent& WXUNUSED(event))
+{
+  Close(TRUE);
+}
+
+class MyApp: public wxApp
+{
+  virtual bool OnInit();
+};
+
+bool MyApp::OnInit()
+{
+  MyFrame *frame = new MyFrame( _T("Hello World"), wxPoint(50,50), wxSize(450,340) );
+  frame->Show(TRUE);
+  SetTopWindow(frame);
+  return TRUE;
+}
+
+BEGIN_EVENT_TABLE(MyFrame, wxFrame)
+EVT_MENU(ID_Quit, MyFrame::OnQuit)
+EVT_MENU(ID_About, MyFrame::OnAbout)
+END_EVENT_TABLE()
+
+IMPLEMENT_APP(MyApp)
