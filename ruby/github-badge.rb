89156bec63106f5b6f52b0f575940f53b7a319ff
Will Roe <willroe@gmail.com>
2009-05-07 22:26:58 +0100

    removing github

--- ruby/github-badge.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/ruby
-
-require 'net/http'
-require 'uri'
-require 'json'
-
-def getUsersInfo(username)
-  JSON.parse Net::HTTP.get(URI.parse("http://github.com/api/v1/json/" + username))
-end
