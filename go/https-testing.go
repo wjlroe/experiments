bc16be5064f41a40e4c46838981ede709484ff1c
William Roe <william.roe@migcan.com>
2010-10-23 19:49:45 +0100

    Add some thing file for testing https with Go. I'm boring myself

---
+++ go/https-testing.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"http"
+	"log"
+)
+
+func main() {
+	// This works
+	r, _, err := http.Get("https://www.google.com")
+	if err != nil {
+		log.Exit(err)
+	}
+	log.Println(r)
+
+	// This doesn't
+	r, _, err := http.Get("https://streaming.campfirenow.com")
+	if err != nil {
+		log.Exit(err)
+	}
+	log.Println(r)
+}
